dd503c44362528c24fdd5e56c691407fb7064d93
kianpu34593 <pujiankun@gmail.com>
2021-01-24 21:19:51 -0500

    syntax update

--- src/surf_generator.py
+++ src/surf_generator.py
@@ -13,21 +13,13 @@ def sym_all_slab(element,max_ind,layers,vacuum_layer):
     slabgenall=generate_all_slabs(bulk_pym,max_ind,layers,vacuum_layer,
                                 lll_reduce=True,center_slab=True,
                                 symmetrize=True,in_unit_planes=True)
-    print('Miller Index','Num of Different Shift')
+    print('Miller Index'+'\t'+'Num of Different Shift(s)')
     slab_M=[]
-    #slab_layer=[]
     for slab in slabgenall:
-        print(miller_index)
-        #slab_M.append(slab.miller_index)
-        #slab_ase=AseAtomsAdaptor.get_atoms(slab)
-        #slab_layer.append(len(np.unique(slab_ase.positions[:,2])))
-    #slab_M_unique = Counter(chain(*slab_M))
-    #print(slab_M_unique)
-    #print(list(slab_M_unique.keys()))
-    # for key in list(slab_M_unique.keys()):
-    #     print(key)
-    #     print(slab_M_unique[key])
-    #     print(str(key)+'\t'+str(slab_M_unique[key]))
+        slab_M.append([slab.miller_index])
+    slab_M_unique = Counter(chain(*slab_M))
+    for key in list(slab_M_unique.keys()):
+        print(str(key)+'\t'+str(slab_M_unique[key]))
 
 
 
