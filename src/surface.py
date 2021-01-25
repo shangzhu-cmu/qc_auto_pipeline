9616e361a7fe7dbeef49340084f992864cd2d356
kianpu34593 <pujiankun@gmail.com>
2021-01-24 22:31:48 -0500

    surf_creator

---
+++ src/surface.py
@@ -0,0 +1,45 @@
+from pymatgen.core.surface import SlabGenerator, generate_all_slabs
+from pymatgen.io.ase import AseAtomsAdaptor
+from ase.db import connect
+from pymatgen.io.cif import CifWriter
+import numpy as np
+from collections import Counter 
+from itertools import chain 
+from pymatgen.analysis.adsorption import plot_slab
+from matplotlib import pyplot as plt
+
+def sym_all_slab(element,max_ind,layers,vacuum_layer):
+    bulk_ase=connect('final_database/bulk.db').get_atoms(name=element)
+    bulk_pym=AseAtomsAdaptor.get_structure(bulk_ase)
+    slabgenall=generate_all_slabs(bulk_pym,max_ind,layers,vacuum_layer,
+                                lll_reduce=True,center_slab=True,
+                                symmetrize=True,in_unit_planes=True)
+    print('Miller Index'+'\t'+'Num of Different Shift(s)')
+    slab_M=[]
+    for slab in slabgenall:
+        slab_M.append([slab.miller_index])
+    slab_M_unique = Counter(chain(*slab_M))
+    for key in list(slab_M_unique.keys()):
+        print(str(key)+'\t'+str(slab_M_unique[key]))
+
+def surf_creator(element,ind,layers,vacuum_layer):
+    bulk_ase=connect('final_database/bulk.db').get_atoms(name=element)
+    bulk_pym=AseAtomsAdaptor.get_structure(bulk_ase)
+    slabgen = SlabGenerator(bulk_pym, ind, layers, vacuum_layer,
+                            center_slab=True,lll_reduce=True,in_unit_planes=True)
+    slabs=slabgen.get_slabs()
+    slabs_symmetric=[slab for slab in slabs if slab.is_symmetric()]
+    if len(slabs_symmetric) == 0:
+        print('No symmetric slab found!')
+    else:
+        print('No.'+'\t'+'No. of Layers'+'\t'+'Angles')
+        fig=plt.figure(figsize=(20,10))
+        for n,slab in enumerate(slabs_symmetric):
+            slab_ase=AseAtomsAdaptor.get_atoms(slab)
+            print(str(n)+'\t'+str(len(np.unique(slab_ase.positions[:,2])))+'\t'+str(slab_ase.get_cell_lengths_and_angles()[3:]))
+            ax=fig.add_subplot(np.ceil(len(slabs_symmetric))/2,2,n)
+            plot_slab(slab,ax,adsorption_sites=False,decay=0.25,window=1)
+            ax.set_title('{}: No. {}'.format(slab.miller_index,n))
+            ax.set_xticks([])
+            ax.set_yticks([])
+
