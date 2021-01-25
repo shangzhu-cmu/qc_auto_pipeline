d86e939d87d838891afddf00fae8925eade9d0a8
kianpu34593 <pujiankun@gmail.com>
2021-01-24 21:01:30 -0500

    syntax update

--- src/surf_generator.py
+++ src/surf_generator.py
@@ -21,7 +21,7 @@ def sym_all_slab(element,max_ind,layers,vacuum_layer):
         slab_layer.append(len(np.unique(slab_ase.positions[:,2])))
     slab_M_unique = Counter(chain(*slab_M))
     for key in list(slab_M_unique.keys()):
-        print(str(key)+'\t'+slab_M_unique[key])
+        print(str(key)+'\t'+str(slab_M_unique[key]))
 
 
 
