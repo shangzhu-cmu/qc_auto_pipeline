ce1ff0e0bd6bedbfab3c33744c864b66f27eca9c
kianpu34593 <pujiankun@gmail.com>
2021-03-14 00:40:19 -0500

    syntax update

--- GPAW_converge/molecule/utils.py
+++ GPAW_converge/molecule/utils.py
@@ -64,6 +64,8 @@ def create_xc_dir(cid,sub_dir=['PBE,BEEF']):
 def mol_pubchem_grabber(cid):
     cid_i=cid[0]
     pure_cid=int(cid_i.split('_')[0])
+    current_dir=os.getcwd()
+    os.chdir(current_dir+'/'+'input_xyz')
     try:
         mol=pubchem_atoms_conformer_search(cid=pure_cid)
     except:
@@ -71,9 +73,9 @@ def mol_pubchem_grabber(cid):
         sys.exit()
     c=pcp.get_compounds(pure_cid,'cid')
     synonyms_name=(c[0].synonyms)[0]
-    #mol_name=synonyms_name.lower().replace(' ','-')
+    mol_name=synonyms_name.lower().replace(' ','-')
     for i,mol_i in enumerate(mol):
-        mol_i.write('input_xyz/'+str(cid[i])+'.xyz',format='xyz')
+        mol_i.write(mol_name+str(cid[i])+'.xyz',format='xyz')
         print("'"+cid+"'",'input xyz is saved successfully!')
 
 
