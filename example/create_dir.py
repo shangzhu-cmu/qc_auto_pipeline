230af5dec1c502294edf2cf8ebd12be9d2450646
Jiankun Pu <kianpu01@gmail.com>
2021-06-17 16:38:20 -0400

    change the structure of this repo

--- example/create_dir.py
+++
@@ -1,9 +0,0 @@
-#create directory before runing convergence test
-import utils as ut
-element_ls=['Cu','LiZn']
-for element in element_ls:
-    ut.create_dir(element,options=['bulk','surf'],
-                surf_struc=['100','110','111'],
-                optimized_parameters=['h','k','sw'],
-                starting_layer=5,
-                init_vac=8)
