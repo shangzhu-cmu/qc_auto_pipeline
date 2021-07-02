a6702daaab5b00a431d284ac595b8f3aae976acd
Jiankun Pu <kianpu01@gmail.com>
2021-07-02 12:00:54 -0400

    syntax error

--- QC_pipeline/GPAW/mol_calc.py
+++ QC_pipeline/GPAW/mol_calc.py
@@ -71,8 +71,7 @@ class GPAW_mol_calculator:
             if convergence_criteria == None:
                 raise RuntimeError('Specify convergence criteria in unoccupied mode.')
             else:
-                parprint(unoccupied_bands)
-                convergence_criteria['bands']=str(unoccupied_bands[number_of_unoccupied_bands_converged])#'CBM+'+str(aboveLUMO)
+                convergence_criteria['bands']=int(unoccupied_bands[number_of_unoccupied_bands_converged])#'CBM+'+str(aboveLUMO)
                 self.atoms, calculator = restart(file_prev+'.gpw')
             calc_bands=calculator.fixed_density(nbands=int(nbands*2.5),
                                                 txt='results/'+cid+'/'+'homo-lumo'+'/'+file_name+'_unoccupied.txt',
